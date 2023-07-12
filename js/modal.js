5a595eb2e8285efaff1e6a64b22f2d404038d905
elias-291356 <ikucher.002@gmail.com>
2023-07-13 01:08:15 +0400

    удаление лишних свойств

--- js/modal.js
+++ js/modal.js
@@ -13,8 +13,8 @@
   }
 })();
 
-const { height: pageHeaderHeight } = document
-  .querySelector(".header")
-  .getBoundingClientRect();
+// const { height: pageHeaderHeight } = document
+//   .querySelector(".header")
+//   .getBoundingClientRect();
 
-document.body.style.paddingTop = `${pageHeaderHeight}px`;
+// document.body.style.paddingTop = `${pageHeaderHeight}px`;
